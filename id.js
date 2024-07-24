4ed09248bc23e88ea746257a40b0de884839cea2
solomon0192 <166137826+solomon0192@users.noreply.github.com>
2024-07-24 17:03:08 +0100

    Add files via upload

---
+++ id.js
@@ -0,0 +1,10 @@
+// document.getElementById('#ham-bug').addEventListener('click', function() {
+//     document.getElementById('#uul').classList.toggle('open');
+// });
+
+
+
+document.getElementById('#ham-bug').onclick = function() {
+   
+    document.getElementById('#uul').style.display = none
+}
